cd0445692c98b7335f2ba952f0dc27af4e1d16c7
Ghiats Abdurahman Rasyid <ghiatsajalah@gmail.com>
2025-05-10 21:19:52 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,14 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('siswa.index');
 });
+
+Route::get('/siswa', [App\Http\Controllers\SiswaController::class, 'index']);
+Route::get('/siswa/create', [App\Http\Controllers\SiswaController::class, 'create']);
+Route::post('/siswa', [App\Http\Controllers\SiswaController::class, 'store']);
+Route::get('/siswa/{id}', [App\Http\Controllers\SiswaController::class, 'show']);
+Route::get('/siswa/{id}/edit', [App\Http\Controllers\SiswaController::class, 'edit']);
+Route::put('/siswa/{id}', [App\Http\Controllers\SiswaController::class, 'update']);
+Route::delete('/siswa/{id}', [App\Http\Controllers\SiswaController::class, 'destroy']);
+
